b825316f9300666cf0b04eaaefb238d393bcd25d
Cristhian Velazco <cristhianvs@yahoo.com>
2023-10-06 14:47:13 -0600

    Update Chat.tsx
    Se cambia el texto de ingles a español del chat

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -548,8 +548,8 @@ const Chat = () => {
                                     className={styles.chatIcon}
                                     aria-hidden="true"
                                 />
-                                <h1 className={styles.chatEmptyStateTitle}>Start chatting</h1>
-                                <h2 className={styles.chatEmptyStateSubtitle}>This chatbot is configured to answer your questions</h2>
+                                <h1 className={styles.chatEmptyStateTitle}>Empiece a chatear</h1>
+                                <h2 className={styles.chatEmptyStateSubtitle}>Este chatbot esta configurado para responder tus preguntas</h2>
                             </Stack>
                         ) : (
                             <div className={styles.chatMessageStream} style={{ marginBottom: isLoading ? "40px" : "0px"}} role="log">
